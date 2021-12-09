870a8ab25a833cdbd725e8fcfda8b57d4b14a7d5
sergio-rins <sergio@gmail.com>
2021-12-09 16:34:27 +0100

    adding footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="FOO_FooterStyle1">
+    <div class="FOO_GroupNames" >
+        <p>carlos1@correo.si</p>
+        <p>carlos2@correo.si</p>
+        <p>sergiox@correo.si</p>
+    </div>
+    <div class="FOO_link">
+        <a href="https://agora.xtec.cat/iesesteveterradas/" target="blank">IES Esteve Terradas</a>
+    </div>
+    <div class="FOO_link">
+        <a href="https://github.com/CarlosMunozRo/DnDCharlieRebolution" target="blank">GitHub</a>
+    </div>
+</footer>

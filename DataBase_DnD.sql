175cc3ec844eebc31b4c4a52035fcbb969947186
Carlos Muñoz Rosendo <carmuny54@gmail.com>
2021-12-12 16:45:39 +0100

    d

--- DataBase_DnD.sql
+++ DataBase_DnD.sql
@@ -15,14 +15,14 @@ CREATE TABLE Armas_PropiedadesArma (
 CREATE TABLE Usuarios (
     UsuarioID INT AUTO_INCREMENT PRIMARY KEY,
     NombreUsuario varchar(30) not null,
-    FechaNacimiento Date not null,
+    FechaNacimiento date not null,
     Password varchar(257) not null,
     Email varchar(50) not null
 );
 CREATE TABLE Armas (
     NombreArma varchar(30) PRIMARY KEY,
-    TipoArma varchar(20) not null,
-    Coste int,
+    TipoArma varchar(100) not null,
+    Coste varchar(20),
     Dano varchar(20) not null,
     TipoDano varchar(20) not null,
     Peso int
@@ -30,7 +30,7 @@ CREATE TABLE Armas (
 CREATE TABLE Armaduras (
     NombreArmadura varchar(30) PRIMARY KEY,
     TipoArmadura varchar(20) not null,
-    Coste int,
+    Coste varchar(20),
     CA int not null,
     MaximoDestreza int,
     RequisitoFuerza int,
@@ -39,14 +39,14 @@ CREATE TABLE Armaduras (
 );
 CREATE TABLE Objetos (
     NombreObjeto varchar(20) PRIMARY KEY,
-    Descripcion varchar(1000) not null,
+    Descripcion varchar(1000),
     Coste varchar(20),
     Peso int
 );
 CREATE TABLE Conjuros (
     NombreConjuro varchar(30) PRIMARY KEY,
     TiempoLanzamiento varchar(30) not null,
-    Alcance varchar(10) not null,
+    Alcance varchar(100) not null,
     Componentes varchar(50) not null,
     Duracion varchar(50) not null,
     Descripcion varchar(5000) not null,
@@ -82,7 +82,7 @@ CREATE TABLE Personajes_Dotes (
     NombreDote varchar(30) not null    
 );
 CREATE TABLE Personajes (
-    PersonajeID int PRIMARY KEY,
+    PersonajeID int AUTO_INCREMENT PRIMARY KEY,
     Nombre varchar(50),
     Clase varchar(20),
     Trasfondo varchar(50),
@@ -113,42 +113,25 @@ CREATE TABLE Trasfondo (
 );
 CREATE TABLE Razas (
     NombreRaza varchar(30) PRIMARY KEY,
-    IncrementoEstadistica varchar(10) not null,
-    Dimension varchar(20) not null,
-    Velocidad varchar(10) not null,
-    Vision varchar(20) not null,
+    IncrementoEstadistica varchar(300) not null,
+    Dimension varchar(20),
+    Velocidad int(10),
+    Vision varchar(300),
     RazaPadre varchar(30)
-);
-CREATE TABLE Razas_Idiomas (
-    NombreRaza varchar(30) not null,
-    NombreIdioma varchar(30) not null
-);
-CREATE TABLE Idiomas (
-    NombreIdioma varchar(30) PRIMARY KEY
 );
 CREATE TABLE Clases (
     NombreClase varchar(30) PRIMARY KEY,
-    Descripcion varchar(200) not null,
+    Descripcion varchar(1000) not null,
     DG int not null,
     CaracteristicaPrimaria varchar(20) not null,
     CompetenciasSalvacion varchar(30) not null,
-    CompetenciasArmas varchar(50),
-    CompetenciasArmaduras varchar(50),
+    CompetenciasArmas varchar(100),
+    CompetenciasArmaduras varchar(200),
     EquipoInicial varchar(200),
     Rasgos varchar(50),
     TrucosConocidos int,
-    ConjurosConocidos int,
-    EspaciosConjuroPorNivel varchar(30)
-);
-CREATE TABLE Clases_EspacioConjuros (
-    NombreClase varchar(30) not null,
-    Id int not null
-);
-CREATE TABLE EspacioConjuros (
-    Id int PRIMARY KEY,
-    NivelClase int not null,
-    NumeroConjuros int not null
-);
+    ConjurosConocidos int);
+
 CREATE TABLE Razas_HabilidadesRaciales (
     NombreRaza varchar(30) not null,
     NombreHabilidadRacial varchar(50) not null
@@ -165,6 +148,32 @@ CREATE TABLE HabilidadesRaciales (
     NombreHabilidadRacial varchar(50) PRIMARY KEY,
     Descripcion varchar(1000)
 );
+CREATE TABLE Razas_Idiomas (
+    NombreRaza varchar(30) not null,
+    NombreIdioma varchar(30) not null
+);
+CREATE TABLE Idiomas (
+    NombreIdioma varchar(30) PRIMARY KEY
+);
+CREATE TABLE Clases_EspacioConjuros (
+    NombreClase varchar(30) not null,
+    Id int not null
+);
+CREATE TABLE EspacioConjuros (
+    Id int PRIMARY KEY,
+    NivelClase int not null,
+    NumeroConjuros int not null
+);
+
+ALTER TABLE Clases_EspacioConjuros
+ADD FOREIGN KEY (NombreClase) REFERENCES Clases(NombreClase);
+ALTER TABLE Clases_EspacioConjuros
+ADD FOREIGN KEY (Id) REFERENCES EspacioConjuros(Id);
+
+ALTER TABLE Razas_Idiomas
+ADD FOREIGN KEY (NombreRaza) REFERENCES Razas(NombreRaza);
+ALTER TABLE Razas_Idiomas
+ADD FOREIGN KEY (NombreIdioma) REFERENCES Idiomas(NombreIdioma);
 
 ALTER TABLE Armas_PropiedadesArma
 ADD FOREIGN KEY (NombreArma) REFERENCES Armas(NombreArma);
@@ -211,35 +220,104 @@ ADD FOREIGN KEY (NombreRaza) REFERENCES Razas(NombreRaza);
 ALTER TABLE Razas_HabilidadesRaciales
 ADD FOREIGN KEY (NombreHabilidadRacial) REFERENCES HabilidadesRaciales(NombreHabilidadRacial);
 
-ALTER TABLE Clases_EspacioConjuros
-ADD FOREIGN KEY (NombreClase) REFERENCES Clases(NombreClase);
-ALTER TABLE Clases_EspacioConjuros
-ADD FOREIGN KEY (Id) REFERENCES EspacioConjuros(Id);
-
-INSERT INTO Usuarios (NombreUsuario,Password,Email)
-VALUES ('carlos','carlos123','carlos@gmail.com');
-
-ALTER TABLE Razas_Idiomas
-ADD FOREIGN KEY (NombreRaza) REFERENCES Razas(NombreRaza);
-ALTER TABLE Razas_Idiomas
-ADD FOREIGN KEY (NombreIdioma) REFERENCES Idiomas(NombreIdioma);
 
 INSERT INTO Usuarios (NombreUsuario,FechaNacimiento,Password,Email)
 VALUES ('carlos','2000-02-28','AC9C2C34C9F7AD52528C3422AF40A66E2E24AAF2A727831255413C9470158984','carlos@gmail.com');
 
+INSERT INTO Clases (NombreClase,Descripcion,DG,CaracteristicaPrimaria,CompetenciasSalvacion,CompetenciasArmas,CompetenciasArmaduras,EquipoInicial,Rasgos,TrucosConocidos,ConjurosConocidos)
+VALUES ('Guerrero','Un maestro del combate marcial, competente con una gran variedad de armas y armaduras.',10,'fuerza o destreza','fuerza y contitucion','armas simples y marciales','todas',null,null,0,0),
+('Mago','Un usuario de magia educado capaz de manipular la estructura de la realidad.',6,'Inteligencia','Inteligencia y sabiduria','Dagas, dorados, hondas, bastones, ballestas ligeras',null,null,null,3,0),
+('Barbaro','La Furia es lo que define al Bárbaro. Una ira desenfrenada, insaciable e irreflexiva. No se trata de una simple emoción, pues esta ira es similar a la ferocidad del depredador acorralado, el asalto implacable de la tormenta o la agitada confusión del mar.',12,'Fuerza','Fuerza y contitucion','Armas sencillas, armas marciales','Todas las armaduras, escudos',null,null,0,0),
+('Bardo','Un Bardo, ya sea este un erudito, un escaldo o un bribón, es capaz de tejer magia a través de sus palabras y su música, inspirando a sus aliados, desmoralizando a sus enemigos, manipulando mentes, creando ilusiones e incluso sanando heridas.',8,'Carisma','Carisma y destreza','Armas sencillas, ballestas de mano, espadas largas, estoques, espadas cortas','Armadura ligera',null,null,2,4),
+('Brujo','Los Brujos buscan el conocimiento que yace escondido tras el tejido mismo del multiverso. A través de pactos forjados con misteriosos seres de poder sobrenatural, desvelan efectos mágicos sutiles y espectaculares a partes iguales. A partir del conocimiento ancestral poseido por entidades como nobles feéricos, demonios, diablos, sagas o los extraños seres del Reino Lejano, los brujos descubren secretos arcanos que refuerzan sus propios poderes.',8,'Carisma','Sabiduria y Carisma','Armas sencillas','Armadura ligera',null,null,2,2),
+('Clerigo','Los clérigos son intermediarios entre el mundo mortal y los remotos planos de los dioses. Tan variopintos como las deidades a las que sirven, estos se esfuerzan por personificar las obras de sus dioses. Un clérigo no es un sacerdote corriente, pues está imbuido de magia divina',8,'Sabiduria','Sabiduria y Carisma','Armas sencillas','Armadura ligera, armadura intermedia, escudos',null,null,3,0),
+('Druida','Un sacerdote de la Antigua Fe, que blande los poderes de la naturaleza la luz de la luna y el crecimiento de las plantas, el fuego y el rayo y que adopta formas animales.',8,'Sabiduria','Inteligencia y sabiduria','Bastones, cimitarras, clavas, dagas, dardos, hoces, hondas, jabalinas, mazas, lanzas','Armadura ligera, armadura intermedia, escudos (los druidas no llevan armadura ni usan escudos hechos de metal)',null,null,2,0),
+('Explorador','Un guerrero que usa la proeza marcial y la magia de la naturaleza para combatir las amenazas en los límites de la civilización.',10,'Destreza y sabiduria','Fuerza y destreza','Armas sencillas, armas marciales','Armadura ligera, armadura intermedia, escudos',null,null,0,0),
+('Hechicero','Un lanzador de conjuros que recurre a la magia inherente de un don o una línea de sangre.',6,'Carisma','Constitucion y Carisma','ballestas ligeras, bastones, dagas, dardos, hondas',null,null,null,4,2),
+('Monje','Un maestro de las artes marciales, que domina el poder del cuerpo en busca de la perfección física y espiritual.',8,'Destreza y sabiduria','Fuerza y destreza','Armas sencillas, espadas cortas',null,null,null,0,0),
+('Paladin','Un guerrero santo atado a un juramento sagrado.',10,'Fuerza y Carisma','Sabiduria y Carisma','Armas sencillas, armas marciales','Todas las armaduras, escudos',null,null,0,0),
+('Picaro','Un rufián que usa sigilo y astucia para superar obstáculos y enemigos.',8,'Destreza','Destreza e Inteligencia','Armas sencillas, ballestas de mano, espadas largas, estoques, espadas cortas','Armadura ligera',null,null,0,0);
+
+INSERT INTO Razas (NombreRaza,IncrementoEstadistica,Dimension,Velocidad,Vision,RazaPadre)
+VALUES ('Elfo','Destreza 2','mediano',30,'vision en la oscuridad',null),
+('Alto Elfo','Inteligencia 1',null,null,null,'Elfo'),
+('Elfo de los Bosques','sabiduria 1',null,null,null,'Elfo'),
+('Elfo Oscuro','Carisma 1',null,null,'vision en la oscuridad superior','Elfo'),
+('Enano','Constitucion 2','mediano',25,'vision en la oscuridad',null),
+('Enano de las Colinas','sabiduria 1',null,null,null,'Enano'),
+('Enano de las Montañas','fuerza 2',null,null,null,'Enano'),
+
+('Gnomo','Inteligencia 2','pequenyo',25,'vision en la oscuridad',null),
+('Gnomo de los Bosques','Destreza 1',null,null,null,'Gnomo'),
+('Gnomo de las Rocas','Constitucion 1',null,null,null,'Gnomo'),
+
+('Humano','Carateristicas 1','mediano',30,null,null),
+('Mediano','Destreza 2','pequenyo',25,null,null),
+('Piesligeros','Carisma 1',null,null,null,'Mediano'),
+('Fornido','Constitucion 1',null,null,null,'Mediano'),
+
+('Semielfo','Carisma 2','mediano',30,'vision en la oscuridad',null),
+('Semiorco','Fuerza 2, Constitucion 1','mediano',30,'vision en la oscuridad',null),
+('Tiefling','Inteligencia 1, Carisma 2','mediano',30,'vision en la oscuridad',null);
+
+INSERT INTO Idiomas (NombreIdioma)
+VALUES ('Comun'),('Elfico'),('Enano'),('Gigante'),('Gnomo'),('Trasgo'),('Mediano'),('Orco'),
+('Abismal'),('Celestial'),('Dragon'),('Habla Profunda'),('Infernal'),('Infracomun'),('Primordial'),('Silvano');
+
+INSERT INTO Razas_Idiomas (NombreRaza, NombreIdioma)
+VALUES ('Elfo','Comun'),('Elfo','Elfico'),
+('Enano','Comun'),('Enano','Enano'),
+('Gnomo','Comun'),('Gnomo','Gnomo'),
+('Humano','Comun'),
+('Mediano','Comun'),('Mediano','Mediano'),
+('Semielfo','Comun'),('Semielfo','Elfico'),
+('Semiorco','Comun'),('Semiorco','Orco'),
+('Tiefling','Comun'),('Tiefling','Infernal');
+
 INSERT INTO Armas (NombreArma,TipoArma,Coste,Dano,TipoDano,Peso)
 VALUES ('Baston','Cuerpo a cuerpo sencilla','2pp','1d6','Contundente',4),
-('Clava','Cuerpo a cuerpo sencilla','1pp','1d4','Contundente',2);
+('Clava','Cuerpo a cuerpo sencilla','1pp','1d4','Contundente',2),
+('Arco corto','Armas a distancia sencillas','25po','1d6','Perforante',2),
+('Ballesta ligera','Armas a distancia sencillas','25po','1d8','Perforante',5),
+('Alabarda','Armas cuerpo a cuerpo marciales','20po','1d10','Cortante',6),
+('Cimitarra','Armas cuerpo a cuerpo marciales','25po','1d6','Cortante',3),
+('Tridente','Armas cuerpo a cuerpo marciales','5po','1d6','Perforante',4),
+('Arco largo','Armas a distancia marciales','50po','1d8','Perforante',2),
+('Ballesta de mano','Armas a distancia marciales','75po','1d6','Perforante',3),
+('Ballesta pesada','Armas a distancia marciales','50po','1d10','Perforante',18);
 
 INSERT INTO PropiedadesArma (NombrePropiedadArma,Descripcion)
 VALUES ('Versatil','Esta arma puede usarse con una o dos manos. Entre paréntesis aparece también un valor de daño: es el daño que inflige cuando se usa para hacer un ataque cuerpo a cuerpo con dos manos.'),
-('Ligera','Un arma ligera es pequeña y fácil de manejar, ideal para luchar con dos armas.');
+('Ligera','Un arma ligera es pequeña y fácil de manejar, ideal para luchar con dos armas.'),
+('Municion','Solo puedes realizar un ataque a distancia con un arma con esta propiedad si tienes la munición requerida. Cada vez que ataques con ella, gastas parte de la munición. Coger la munición de una aljaba, de un estuche o de otro contenedor forma parte del ataque (necesitas una mano libre para cargar un arma de una mano). Al final del combate, puedes recuperar la mitad de la munición que hayas gastado dedicando 1 minuto a buscar por el campo de batalla.Si usas un arma que tiene la propiedad «munición» para realizar un ataque cuerpo a cuerpo, considera que el arma es improvisada (ver «Armas improvisadas» a continuación). Las ondas deben cargarse para infligir daño de este modo.'),
+('Dos manos','Esta arma requiere que la sostengas con dos manos cuando atacas con ella.'),
+('Alcance','Esta arma añade 5 pies a tu alcance cuando atacas con ella, así como cuando determinas tu alcance para hacer un ataque de oportunidad con ella.'),
+('Pesada','El tamaño y el peso de un arma pesada hacen que sea demasiado grande para que las criaturas Pequeñas la utilicen con eficacia, por lo que tienen desventaja en las tiradas de ataque que realicen con estas armas.'),
+('Sutil','Cuando hagas un ataque con un arma sutil, elige si utilizas tu modificador por Fuera o tu modificador por Destreza para la tirada de ataque y de daño. Debes usar el mismo modificador para ambas tiradas.'),
+('Arrojadiza','Si un arma tiene esta propiedad, puedes lanzarla para hacer un ataque a distancia. Si el arma es de combate cuerpo a cuerpo, usa el mismo modificador por característica que utilizarías para hacer un ataque cuerpo a cuerpo con ella, tanto para la tirada de ataque como para la de daño. Por ejemplo, si arrojas un hacha, usas tu Fuerza, pero si arrojas una daga, puedes usar o tu Fuerza o tu Destreza, dado que la daga tiene la propiedad «sutil».'),
+('De carga','Debido al tiempo que se necesita para cargar esta arma, solo puedes disparar una pieza de munición en la acción, acción adicional o reacción en que la dispares, independientemente del número de ataques que puedas hacer normalmente.'),
+('Distancia','Un arma que puede usarse para hacer un ataque a distancia tiene un paréntesis en el que dice «a distancia» después de las propiedades «munición» o «arrojadiza». También aparecen dos números: el primero es la distancia normal del arma en pies, y el segundo, su distancia larga. Cuando ataques a un objetivo que está más lejos de la distancia habitual, tienes desventaja en la tirada de ataque. No puedes atacar a un objetivo que esté más lejos que la distancia larga.');
 
-INSERT INTO Armas_PropiedadesArma VALUES ('Baston','Versatil'),('Clava','Ligera');
+INSERT INTO Armas_PropiedadesArma VALUES ('Baston','Versatil'),('Clava','Ligera'),
+('Arco corto','Municion'),('Ballesta ligera','Municion'),
+('Alabarda','Pesada'),('Alabarda','Alcance'),('Alabarda','Dos manos'),
+('Cimitarra','Sutil'),('Cimitarra','Ligera'),
+('Tridente','Arrojadiza'),('Tridente','Versatil'),
+('Arco largo','Municion'),
+('Ballesta de mano','Municion'),('Ballesta de mano','ligera'),('Ballesta de mano','De carga'),
+('Ballesta pesada','Municion'),('Ballesta pesada','Pesada'),('Ballesta pesada','De carga'),('Ballesta pesada','Dos manos');
 
 INSERT INTO Armaduras (NombreArmadura,TipoArmadura,Coste,CA,MaximoDestreza,RequisitoFuerza,Sigilo,Peso)
-VALUES ('Acolchada','Ligera','5po',11,-1,null,'desventaja',8),
-('Pieles','Media','10po',12,2,null,null,12);
+VALUES ('Acolchada','Ligera','5po',11,null,null,'desventaja',8),
+('Cuero','Ligera','10po',11,null,null,null,10),
+('Cuero tachonao','Ligera','45po',12,null,null,null,13),
+('Pieles','Intermedia','10po',12,2,null,null,12),
+('Camisote de malla','Intermedia','50po',13,2,null,null,20),
+('Cota de escamas','Intermedia','50po',14,2,null,'desventaja',45),
+('Coraza','Intermedia','400po',14,2,null,null,20),
+('Armadura de anillas','Pesada','30po',14,null,null,'desventaja',40),
+('Cota de malla','Pesada','75po',16,null,15,'desventaja',55),
+('Armadura laminada','Pesada','200po',17,null,15,'desventaja',60);
 
 INSERT INTO Objetos (NombreObjeto,Descripcion,Coste,Peso)
 VALUES ('Raciones(1 dia)',null,'5pp',2),('Saco de dormir',null,'1po',7);
@@ -252,9 +330,9 @@ INSERT INTO Dotes (NombreDote,Requisito,Descripcion)
 VALUES ('acechador','Destreza 13 o super(ior','Eres un experto en ocultarte en las sombras. Ganas los siguientes beneficios: • Puedes intentar esconderte cuando estás en penumbra de la criatura de la cual te estás ocultando. • Cuando estás escondido de una criatura y fallas con un arma a distancia, hacer el ataque no delatará tu posición. • La luz tenue no supone una desventaja para tus tiradas de Sabiduría (Percepción) si estas se basan en la vista.'),
 ('Actor',null,'Eres experimentado en la imitación y en el arte del drama. Ganas los siguientes beneficios: • Incrementa tu puntuación de Carisma en 1, hasta un máximo de 20. • Tienes ventaja en las tiradas de Carisma (Engañar) y Carisma (Interpretar) cuando intentas hacerte pasar por otra persona. • Puedes imitar el habla de otra persona o los sonidos hechos por otras criaturas. Debes haber oído a la persona hablar, o haber escuchado a la criatura haber hecho el sonido, por al menos un minuto. Una tirada exitosa de Sabiduría (Averiguar Intenciones) contra tu tirada de Carisma (Engañar) permite a alguien o algo que escucha determinar que el efecto es falso.');
 
-INSERT INTO Clases (NombreClase,Descripcion,DG,CaracteristicaPrimaria,CompetenciasSalvacion,CompetenciasArmas,CompetenciasArmaduras,EquipoInicial,Rasgos,TrucosConocidos,ConjurosConocidos)
-VALUES ('Guerrero','Un maestro del combate marcial, competente con una gran variedad de armas y armaduras.',10,'fuerza o destreza','fuerza y contitucion','Todas las armaduras, escudos, armas simples y marciales.','todas',null,null,0,0),
-('Mago','Un usuario de magia educado capaz de manipular la estructura de la realidad.',6,'Inteligencia','Inteligencia y sabiduria','Dagas, dorados, hondas, bastones, ballestas ligeras',null,null,null,3,6);
-
-INSERT INTO Razas (NombreRaza,IncrementoEstadistica,Dimension,Velocidad,Vision,RazaPadre)
-VALUES ('Elfo','Destreza 2','mediano',30,'vision en la oscuridad',null),('Alto Elfo','Inteligencia 1',null,null,null,'Elfo');
+INSERT INTO Personajes (Nombre,Clase,Trasfondo,Raza,Alineamiento,Experiencia,RasgosPersonalidad,Ideales,Vinculos,Defectos,Nivel,PGMaximos,CA,Fuerza,Destreza,Constitucion,inteligencia,Sabiduria,Carisma,RasgosAtributos)
+VALUES ('Legolas','Guerrero',null,'Alto Elfo',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null),
+('Gandalf','Mago',null,'Piesligeros',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null),
+('Sauron','Hechicero',null,'Semielfo',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null),
+('Elrond','Picaro',null,'Semiorco',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null),
+('Boromir','Paladin',null,'Humano',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null);
--- Scripts/crear-ficha.js
+++ Scripts/crear-ficha.js
@@ -15,13 +15,11 @@ var formNum=0;
 
 
 function create_sheet(){
-    $("#autoForm").append("<div id='form"+formNum+"' class='form_input '></div>")
-    $("#form"+formNum).append("<label for='sh_name'>Nom: </label>")
-    $("#form"+formNum).append("<input id='sh_name'type='text'></input>")
-    $("#form"+formNum).append("<div id='forButton'></div>")
+    $("#autoForm").append("<div id='form1' class='form_input '></div>")
+    $("#form1").append("<label for='sh_name'>Nom: </label>")
+    $("#form1").append("<input id='sh_name'type='text'></input>")
+    $("#form1").append("<div id='forButton'></div>")
     $("#forButton").append("<a class='BTN_A_ST2_Pequeño icono-animation-delante' onclick='crearFormRazas()'><i class='fas fa-arrow-right'></i></a>")
-
-    formNum++;
 }
 
 function crearFormRazas(){
@@ -31,9 +29,14 @@ function crearFormRazas(){
         return;
     }
 
-    
+    if($('#form1 input').val()==''){
+        return;
+    }
+
+    $('#form1 a').toggleClass('hidden');
 
     $('#form1 input').attr('readonly','true');
+
 
 
     razas.sort(function(a, b) {
@@ -81,17 +84,15 @@ function crearFormRazas(){
     }
 
     $('#form2').append("<a class='BTN_A_ST1_Pequeño icono-animation-atras' onclick='removeRazas()'><i class='fas fa-undo-alt'></i></a>");
-
-
     $('#form2').append("<a class='BTN_A_ST2_Pequeño icono-animation-delante' onclick='crearFormClases()'><i class='fas fa-arrow-right'></i></a>");
-
-
 
 }
 
 function crearFormClases(){
 
     $('#form2 select').attr('disabled','true');
+
+    $('#form2 a').toggleClass('hidden');
 
     $('form#autoForm').append($('<div id="form3"></div>').addClass("form_input"));
 
@@ -105,30 +106,18 @@ function crearFormClases(){
 
     $('#form3').append("<a class='BTN_A_ST1_Pequeño icono-animation-atras' onclick='removeClases()'><i class='fas fa-undo-alt'></i></a>");
 
-    $('#form3').append("<a class='BTN_A_ST2_Pequeño icono-animation-delante' onclick='crearFormRazas()'><i class='fas fa-arrow-right'></i></a>");
+    $('#form3').append("<a class='BTN_A_ST2_Pequeño icono-animation-delante' onclick='skill_points()'><i class='fas fa-arrow-right'></i></a>");
 
 
 }
-
-
-function removeRazas(){
-
-    $('#form1 input').removeAttr("readonly");
-
-    $('#form2').remove();
-
-
-}
-
-function removeClases(){
-
-    $('#form2 select').removeAttr("disabled");
-
-    $('#form3').remove();
-
-};
 
 function skill_points(){
+
+
+    $('#form3 select').attr('disabled','true');
+
+    $('#form3 a').toggleClass('hidden');
+
     $("#autoForm").append("<div id='skillsform' class='form_input '></div>")
     $("#skillsform").append("<p id='puntosres'>27</p>")
     $("#skillsform").append("<label for='d_fuerza'>Fuerza</label>")
@@ -143,8 +132,35 @@ function skill_points(){
     $("#skillsform").append("<input id='d_sabi'type='number' value='8' min='8' max='15'></input>")
     $("#skillsform").append("<label for='d_carism'>Carisma</label>")
     $("#skillsform").append("<input id='d_carism'type='number' value='8' min='8' max='15'></input>")
-    
-    $("#skillsform").append("<div id='forButton"+formNum+"' class='input_butoni'></div>")
-    $("#forButton"+formNum).append("<a class='BTN_NoA_ST2_Pequeño icono-animation-delante'><i class='fas fa-arrow-right'></i></a>")
-    formNum++;
+    $("#skillsform").append("<div id='forButton4' class='input_butoni'></div>")
+    $("#forButton4").append("<a class='BTN_A_ST1_Pequeño icono-animation-atras' onclick='removeSkillPoints()'><i class='fas fa-undo-alt'></i></a>");
+
+    $("#forButton4").append("<a class='BTN_A_ST2_Pequeño icono-animation-delante'><i class='fas fa-arrow-right'></i></a>")
+
+}
+
+function removeRazas(){
+
+    $('#form1 input').removeAttr("readonly");
+    $('#form1 a').toggleClass("hidden");
+
+    $('#form2').remove();
+
+
+}
+
+function removeClases(){
+
+    $('#form2 select').removeAttr("disabled");
+    $('#form2 a').toggleClass("hidden");
+    $('#form3').remove();
+
+};
+
+function removeSkillPoints(){
+
+    $('#form3 select').removeAttr("disabled");
+    $('#form3 a').toggleClass("hidden");
+    $('#skillsform').remove();
+
 }

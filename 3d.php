e0099748a95f9658fbf15898d864c4d84c66efe1
Carlos Muñoz <carmuny54@gmail.com>
2021-12-15 16:38:16 +0100

    Add 3D

---
+++ 3d.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="./Scripts/jquery.min.js" ></script>
+    <link rel="stylesheet" href="styles.css">
+    <script src="./Scripts/cannon.js"></script>
+</head>
+<body>
+
+    <canvas id="3d-bgk"></canvas>
+    <div id="ThreeJS" style="position: absolute; left:0px; top:0px"></div>
+
+    <!-- <script type="module" src="./Scripts/3d_2.0.js"></script> -->
+    <script type="module" src="./Scripts/3d.js"></script>
+</body>
+</html>
